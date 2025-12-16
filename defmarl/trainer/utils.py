a7bd1ff5c1185e126315f7bd3cbdd8e6363bd036
yangxiaoke <740365168@qq.com>
2025-12-16 11:36:23 +0800

    增加了适配CBFs训练方法中多计算一步的rollout和eval方法

--- defmarl/trainer/utils.py
+++ defmarl/trainer/utils.py
@@ -71,6 +71,48 @@ def rollout(
     return rollout_data
 
 
+def rollout_p1step(
+        env: MultiAgentEnv,
+        actor: Callable,
+        init_rnn_state: Array,
+        key: PRNGKey,
+        gamma: float,
+        init_graph: Optional[GraphsTuple] = None
+) -> Rollout:
+    """
+    与rollout几乎一致，只是最后多rollout一步给需要k+1状态的reward或cost计算
+    """
+    key_x0, key_z0, key = jax.random.split(key, 3)
+    if init_graph is None:
+        init_graph = env.reset(key_x0)
+    z0 = jax.random.uniform(key_z0, (1, 1), minval=-env.reward_max, maxval=-env.reward_min)
+
+    z_key, key = jax.random.split(key, 2)
+    rng = jax.random.uniform(z_key, (1, 1))
+    z0 = jnp.where(rng > 0.7, -env.reward_max, z0)  # use z min
+    z0 = jnp.where(rng < 0.2, -env.reward_min, z0)  # use z max
+
+    z0 = jnp.repeat(z0, env.num_agents, axis=0)
+
+    def body(data, key_):
+        graph, rnn_state, z = data
+        action, log_pi, new_rnn_state = actor(graph, z, rnn_state, key_)
+        next_graph, reward, cost, goal, done, info = env.step(graph, action)
+
+        # z dynamics
+        z_next = (z + reward) / gamma
+        z_next = jnp.clip(z_next, -env.reward_max, -env.reward_min)
+
+        return ((next_graph, new_rnn_state, z_next),
+                (graph, action, rnn_state, reward, cost, goal, done, log_pi, next_graph, z))
+
+    keys = jax.random.split(key, env.max_episode_steps+1)
+    _, (graphs, actions, rnn_states, rewards, costs, goals, dones, log_pis, next_graphs, zs) = (
+        jax.lax.scan(body, (init_graph, init_rnn_state, z0), keys, length=env.max_episode_steps+1))
+    rollout_data = Rollout(graphs, actions, rnn_states, rewards, costs, goals, dones, log_pis, next_graphs, zs)
+    return rollout_data
+
+
 def eval_rollout(
         env: MultiAgentEnv,
         actor: Callable,
@@ -106,6 +148,48 @@ def eval_rollout(
                      (init_graph, init_actor_rnn_state, init_Vh_rnn_state),
                      keys,
                      length=env.max_episode_steps))
+    rollout_data = Rollout(graphs, actions, actor_rnn_states, rewards, costs, goals, dones, log_pis, next_graphs, zs)
+    return rollout_data
+
+
+def eval_rollout_p1step(
+        env: MultiAgentEnv,
+        actor: Callable,
+        init_actor_rnn_state: Array,
+        key: PRNGKey,
+        init_Vh_rnn_state: Optional[Array] = None,
+        z_fn: Optional[Callable] = None,
+        stochastic: bool = False
+):
+    """
+    与eval_rollout几乎一致，只是最后多rollout一步给需要k+1状态的reward或cost计算
+    """
+    key_x0, key = jax.random.split(key)
+    init_graph = env.reset(key_x0)
+    z0 = jax.random.uniform(key, (env.num_agents, 1), minval=-env.reward_max, maxval=-env.reward_min)
+
+    def body(data, key_):
+        graph, actor_rnn_state, Vh_rnn_state = data
+        if z_fn is not None:
+            z, Vh_rnn_state = z_fn(graph, Vh_rnn_state)
+            z_max = np.max(z, axis=0)
+            z = jnp.repeat(z_max[None], env.num_agents, axis=0)
+        else:
+            z = z0
+        if not stochastic:
+            action, actor_rnn_state = actor(graph, z, actor_rnn_state)
+        else:
+            action, actor_rnn_state = actor(graph, z, actor_rnn_state, key_)
+        next_graph, reward, cost, goal, done, info = env.step(graph, action)
+        return ((next_graph, actor_rnn_state, Vh_rnn_state),
+                (graph, action, actor_rnn_state, reward, cost, goal, done, None, next_graph, z))
+
+    keys = jax.random.split(key, env.max_episode_steps)
+    _, (graphs, actions, actor_rnn_states, rewards, costs, goals, dones, log_pis, next_graphs, zs) = (
+        jax.lax.scan(body,
+                     (init_graph, init_actor_rnn_state, init_Vh_rnn_state),
+                     keys,
+                     length=env.max_episode_steps+1))
     rollout_data = Rollout(graphs, actions, actor_rnn_states, rewards, costs, goals, dones, log_pis, next_graphs, zs)
     return rollout_data
 
